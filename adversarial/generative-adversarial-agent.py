4285d0903227bd124cd5b07d137ce9b405c9372f
Oswald Berthold <opt@sdfk.de>
2017-09-07 01:10:08 +0200

    testing variants

--- adversarial/generative-adversarial-agent.py
+++ adversarial/generative-adversarial-agent.py
@@ -7,8 +7,10 @@ import argparse
 import numpy as np
 import scipy.signal as signal
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
 
 from smp_base.models_actinf import smpGMM, smpKNN
+from smp_base.learners import smpSHL
 
 def noisef(x):
     return np.random.normal(0, 1.0, size = x.shape) * 0.001
@@ -78,7 +80,9 @@ def main_baseline(args):
     # gs.transf = transf0
     gs.transf = np.random.choice([transf0, transf1, transf2])
 
-    mdl_cls = smpGMM
+    # mdl_cls = smpGMM
+    # mdl_cls = smpKNN
+    mdl_cls = smpSHL
     
     # models
     m_g = mdl1(args)
@@ -89,8 +93,14 @@ def main_baseline(args):
     # error
     m_e_conf = mdl_cls.defaults
     m_e_conf.update({
-        'idim': args.dim, 'odim': args.dim, 'n_neighbors': 5,
-        'prior': 'random', 'prior_width': 1.0, 'fit_interval': 100})
+        'idim': args.dim, 'odim': args.dim, 'n_neighbors': 8,
+        'prior': 'random', 'prior_width': 1.0, 'fit_interval': 100,
+        'eta': 1e-3, 'w_input': 1.0, 'w_bias': 0.0, 'modelsize': 200,
+        'theta_state': 0.02, 'lrname': 'FORCE', 'spectral_radius': 0.01,
+        'tau': 1.0, 'alpha': 1.0, 'wgt_thr': 10, 'mixcomps': 12, 'oversampling': 1,
+        'visualize': True, 'input_coupling': 'normal',
+        'sigma_mu': 5e-3, 'sigma_sig': 5e-9, 'sigma_pi': 5e-4
+        })
     m_e = mdl_cls(conf = m_e_conf)
     
     # inverse
@@ -101,14 +111,22 @@ def main_baseline(args):
     m_i_conf = mdl_cls.defaults
     m_i_conf.update({
         'idim': args.dim * 2, 'odim': args.dim, 'n_neighbors': 5,
-        'prior': 'random', 'prior_width': 0.1, 'fit_interval': 100})
+        'prior': 'random', 'prior_width': 0.1, 'fit_interval': 100,
+        'eta': 1e-3, 'w_input': 1.0, 'w_bias': 0.0, 'modelsize': 200,
+        'theta_state': 0.02, 'lrname': 'FORCE', 'spectral_radius': 0.01,
+        'tau': 1.0, 'alpha': 1.0, 'wgt_thr': 10, 'mixcomps': 12, 'oversampling': 1,
+        'visualize': True, 'input_coupling': 'normal',
+        'sigma_mu': 5e-3, 'sigma_sig': 5e-9, 'sigma_pi': 5e-3
+        })
     m_i = mdl_cls(conf = m_i_conf)
+    # m_i = smpGMM(conf = m_i_conf)
 
     X = np.zeros((args.dim, args.numsteps))
     X_ = np.zeros((args.dim, args.numsteps))
     Y = np.zeros((args.dim, args.numsteps))
     Y_ = np.zeros((args.dim, args.numsteps))
     
+    e_X_ = np.zeros((args.dim, args.numsteps))
     E_argmin_ = np.zeros((args.dim, args.numsteps))
     E_min_ = np.zeros((args.dim, args.numsteps))
     X_e_min_ = np.zeros((args.dim, args.numsteps))
@@ -116,14 +134,20 @@ def main_baseline(args):
     E_max_ = np.zeros((args.dim, args.numsteps))
     X_e_max_ = np.zeros((args.dim, args.numsteps))
 
+    fig = plt.figure()
+    fig.show()
     plt.ion()
-    plt.subplot(131)
-    plt.title('transfer = %s' % (gs.transf))
+    gspc = GridSpec(2, 2)
+    ax = fig.add_subplot(gspc[0,0])
+    ax1 = fig.add_subplot(gspc[0,1])
+    ax2 = fig.add_subplot(gspc[1,0])
+    ax3 = fig.add_subplot(gspc[1,1])
+    ax.set_title('transfer = %s' % (gs.transf))
     t = np.linspace(-1.2, 1.2, 101)
-    plt.plot(t, gs.transf(t))
-    plt.gca().set_ylim([-1.3, 1.3])
-    plt.draw()
-    plt.pause(1e-9)
+    ax.plot(t, gs.transf(t))
+    ax.set_ylim([-1.3, 1.3])
+    # plt.draw()
+    # plt.pause(1e-9)
 
     # goal sampler
     # inverse model
@@ -132,19 +156,24 @@ def main_baseline(args):
     for i in range(1, args.numsteps):
         
         # X_[...,[i]] = m_g.step(X[...,[-1]], x)
-        if i % 10 == 0:
+        resample_interval = 20
+        if i % resample_interval == 0:
             # X_[...,[i]] = np.random.uniform(-1.5, 1.5, size = (args.dim, 1))
-            if (i/10) % 2 == 1:
+            if (i/resample_interval) % 2 == 1:
                 print "sampling cooperative"
                 mu = X_e_min_[...,[i-1]]
                 sig = E_min_[...,[i-1]]
                 sig = 0.5
+                e_ = np.random.uniform(0, 0.2, size = X[...,[i]].shape)
             else:
                 print "sampling adversarial"
                 mu = X_e_max_[...,[i-1]]
                 sig = E_max_[...,[i-1]]
                 sig = 0.1
-            X_[...,[i]] = np.random.normal(mu, sig)
+                e_ = np.random.uniform(0.2, 1.0, size = X[...,[i]].shape)
+            sample = m_e.predict(e_) # + noisef(e_) * np.square(e_)
+            X_[...,[i]] = sample
+            # X_[...,[i]] = np.random.normal(mu, sig)
         else:
             X_[...,[i]] = X_[...,[i-1]].copy()
             
@@ -152,12 +181,13 @@ def main_baseline(args):
 
         # Y[...,[i]] = np.random.uniform(-1, 1, size = (args.dim, 1))
         # Y[...,[i]] = Y[...,[i-1]] + (np.random.normal(0, 1, size = (args.dim, 1)) * 0.1)
-        nu = np.random.normal(0, 1, size = (args.dim, 1)) * 0.0
+        nu = np.random.normal(0, 1, size = (args.dim, 1)) * 0.02
         # print "gs.x", gs.x, "nu", nu
         # y = gs.x + nu
         
         X_m_i = np.vstack((X_[...,[i]], X[...,[i-1]]))
-        Y_[...,[i]] = np.tanh(2.0 * m_i.predict(X_m_i.T)) + nu
+        # Y_[...,[i]] = np.tanh(1.0 * m_i.predict(X_m_i.T)) + nu
+        Y_[...,[i]] = 1.0 * m_i.predict(X_m_i.T) + nu
         # print "Y_", Y_[...,[i]]
         
         # print "y", y
@@ -169,9 +199,10 @@ def main_baseline(args):
 
         e = X_[...,[i]] - X[...,[i]]
         e_X = np.tanh(np.square(e))
+        e_X_[...,[i]] = e_X
 
         # fit error
-        m_e.fit(X = e_X.T, y = X[...,[i]].T)
+        m_e.fit(X = e_X.T, y = X[...,[i]].T)#, update = True)
 
         # print "error sampling"
         E_ = np.random.uniform(0, 1, size = (e_X.shape[0], 10))
@@ -195,25 +226,36 @@ def main_baseline(args):
         # print "fitting inverse"
         
         # fit inverse
-        X_m_i = np.vstack((X[...,1:i], X[...,0:i-1]))
-        Y_m_i = Y_[...,1:i]
-        # print "Xmi", X_m_i
-        # print "Ymi", Y_m_i
-        if i > 0 and i % 100 == 0:
-            m_i.fit(X = X_m_i.T, y = Y_m_i.T)
+        
+        # X_m_i = np.vstack((X[...,1:i], X[...,0:i-1]))
+        # Y_m_i = Y_[...,1:i]
+        # # print "Xmi", X_m_i
+        # # print "Ymi", Y_m_i
+        
+        # if i > 0 and i % 100 == 0:
+        #     m_i.fit(X = X_m_i.T, y = Y_m_i.T)
+        
+        X_m_i = np.vstack((X[...,[i]], X[...,[i-1]]))
+        Y_m_i = Y_[...,[i]]
+        m_i.fit(X = X_m_i.T, y = Y_m_i.T)#, update = True)
 
-        if i % 100 == 0:
-            plt.subplot(132)
-            plt.gca().clear()
-            plt.plot(X_.T, '-o', alpha = 0.5, label = 'X_')
-            plt.plot(X.T, '-o', alpha = 0.5, label = 'X')
-            plt.plot(Y_.T, '-x', alpha = 0.5, label = 'Y')
-            plt.legend()
-            plt.subplot(133)
-            plt.gca().clear()
-            plt.plot(E_min_.T, X_e_min_.T, 'o', alpha = 0.5, label = 'X_e_min')
-            plt.plot(E_max_.T, X_e_max_.T, 'o', alpha = 0.5, label = 'X_e_max')
-            plt.legend()
+        if i % 100 == 0 or i == (args.numsteps - 1):
+            ax1.clear()
+            ax1.plot(e_X_.T, '-o', alpha = 0.5, label = 'e_X_')
+            
+            ax2.clear()
+            ax2.plot(X_.T, '-o', alpha = 0.5, label = 'X_')
+            ax2.plot(X.T, '-o', alpha = 0.5, label = 'X')
+            ax2.plot(Y_.T, '-x', alpha = 0.5, label = 'Y')
+            ax2.legend()
+            ax3.clear()
+            ax3.plot(
+                E_min_.T[i-100:i],
+                X_e_min_.T[i-100:i], 'o', alpha = 0.5, label = 'X_e_min')
+            ax3.plot(
+                E_max_.T[i-100:i],
+                X_e_max_.T[i-100:i], 'o', alpha = 0.5, label = 'X_e_max')
+            ax3.legend()
             plt.draw()
             plt.pause(1e-9)
             
@@ -228,8 +270,11 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--dim',  type=int, default=1, help = 'Number of system dimensions [1]')
     parser.add_argument('-n', '--numsteps',  type=int, default=1000, help = 'Number of steps [1000]')
     parser.add_argument('-o', '--order',  type=int, default=0, help = 'System order [0], 0: kinematic, 1: first order, 2: second order')
+    parser.add_argument('-s', '--seed',   type=int, default=0, help = 'Random seed [0]')
 
     args = parser.parse_args()
 
+    np.random.seed(args.seed)
+    
     if args.mode == 'baseline':
         main_baseline(args)
